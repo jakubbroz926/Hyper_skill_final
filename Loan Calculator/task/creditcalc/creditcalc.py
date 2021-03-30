ecf293f697d46a6e71fb35b2bd9c432e766675dd
JAKUB BROZ <Broz.Jk@gmail.com>
2021-03-30 11:39:47 +0200

    Creating class variables a class functions

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -5,49 +5,21 @@ import argparse
 class Loan:
 
     def __init__(self, args):
-        self.type_of_counting = input("What do you want to calculate?\n"
-                                      "type 'n' for number of monthly payments\n"
-                                      "type 'a' for the monthly payment:\n"
-                                      "type 'p' for loan principal:\n")
-        if self.type_of_counting.lower() == "n":
-            print(self.months())
-        elif self.type_of_counting.lower() == "a":
-            print(self.annuity())
-        elif self.type_of_counting.lower() == "p":
-            print(self.principal_loan())
+        for key in args:
+            setattr(self, key, args[key])
+        self.choice()
 
-    @staticmethod
-    def months():
-        principal = int(input("Enter your principal loan:"))
-        payment = int(input("Enter your monthly payment:"))
-        interest = float(input("Enter the loan interest:"))
-        i = interest / (12 * 100)
-        n = math.log(payment / (payment - i * principal), 1 + i)
-        n_y, n_m = (math.ceil(n) // 12), (math.ceil(n) - 12 * (math.ceil(n) // 12))
-        if n_y == 0:
-            return f"It will take {n_m} months to repay this loan!"
-        elif n_m == 0:
-            return f"It will take {n_y} years to repay this loan!"
+    def choice(self):
+        if self.__getattribute__("type") == "diff":
+            self.diff()
         else:
-            return f"It will take {n_y} years and {n_m} months to repay this loan!"
+            self.annuity()
 
-    @staticmethod
-    def annuity():
-        principal = int(input("Enter your principal loan:"))
-        months = int(input("Enter the number of periods:"))
-        interest = float(input("Enter the loan interest:"))
-        i = interest / (12 * 100)
-        an = math.ceil(principal * ((i * (1 + i) ** months) / ((1 + i) ** months - 1)))
-        return f"Your monthly payment = {an}!"
+    def diff(self):#first write diff method
+        print(self.__getattribute__("principal"))
 
-    @staticmethod
-    def principal_loan():
-        payment = float(input("Enter the annuity payment:"))
-        months = int(input("Enter the number of periods:"))
-        interest = float(input("Enter the loan interest:"))
-        i = interest / (12 * 100)
-        principal = round(payment / ((i * (1 + i) ** months) / ((1 + i) ** months - 1)))
-        return f"Your loan principal = {principal}!"
+    def annuity(self):
+        print(self.__getattribute__("payment"))
 
 
 def check_args(args):
