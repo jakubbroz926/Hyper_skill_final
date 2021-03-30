1af570cfe5ccf68bb37dbee926da9e4ad41c2d7b
JAKUB BROZ <Broz.Jk@gmail.com>
2021-03-30 12:56:32 +0200

    Counting functions complete

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -7,22 +7,44 @@ class Loan:
     def __init__(self, args):
         for key in args:
             setattr(self, key, args[key])
-        self.choice()
+        self.counting_type()
 
-    def choice(self):
+    def counting_type(self):
         if self.__getattribute__("type") == "diff":
             self.diff()
         else:
             self.annuity()
 
-    def diff(self):#first write diff method
-        print(self.__getattribute__("principal"))
+    def diff(self):
+        P = self.__getattribute__("principal")
+        n = self.__getattribute__("periods")
+        i = self.__getattribute__("interest")/(12*100)
+        total = 0
+        for m in range(1, n+1):
+            dm = math.ceil(P/n + i*(P - (P * (m-1)/n)))
+            total += dm
+            print(f"Month {m}: payment is {dm}")
+        print(f"Overpayment = {total-P}")
 
     def annuity(self):
-        print(self.__getattribute__("payment"))
+        if "periods" not in self.__dict__:
+            P = self.__getattribute__("principal")
+            p = self.__getattribute__("payment")
+            i = self.__getattribute__("interest")/(12 * 100)
+            n = math.log(p / (p - i * P), 1 + i)
+            n_y = math.ceil(n)
+            print(f"It will take {n_y//12} years to repay this loan!")
+            print(f"Overpayment = {n_y*p-P} ")
+        else:
+            p = self.__getattribute__("payment")
+            n = self.__getattribute__("periods")
+            i = self.__getattribute__("interest")/(12 * 100)
+            principal = math.ceil(p / ((i * (1 + i) ** n) / ((1 + i) ** n - 1)))
+            print(f"Your loan principal = {principal}!")
+            print(f"Overpayment = {math.ceil(n * p - principal)}")
 
 
-def check_args(args):
+def check_args(args):#rewrite results of conditions
     remove = [k for k, v in args.items() if v is None]
     for key in remove:
         del args[key]
