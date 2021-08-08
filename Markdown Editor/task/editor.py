09bbcf207d2f90aef76488c2ace51c1a5c8a3de8
BrozJakub <Broz.Jk@gmail.com>
2021-08-08 17:12:13 +0200

    Final task completed

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -35,8 +35,28 @@ def header():
         else:
             print("The level should be within the range of 1 to 6")
             continue
-
-
+def n_rows():
+    n_rows = int(input("Number of rows: "))
+    while n_rows <= 0:
+        print("The number of rows should be greater than zero")
+        n_rows = int(input("Number of rows: "))
+    return n_rows
+def dered():  #i didnt want change function list() so I picked string which is contained in both list formatters
+    lines = [input(f"{i}.Row ")for i in range(1,n_rows()+1)]
+    ordered_line = ""
+    for i,line in enumerate(lines):
+        if "un" in ent:
+            ordered_line += f"* {line}\n"
+        else:
+            ordered_line += f"{i+1}. {line}\n"
+    return ordered_line
+    
+def done(text):
+    file = open("output.md","w+")
+    file.write(text)
+    file.close()
+    quit()
+    
 def link():
     label = input("Label: ")
     url = input("URL: ")
@@ -49,22 +69,27 @@ def inline_code():
 def main():
     line_of_text = ""
     entry()
-    viable_commands = ["plain", "bold", "italic", "header", "link"]
+    viable_commands = ["plain", "bold", "italic", "header", "link","ordered-list","unordered-list"]
     while True:
         if ent in viable_commands:
-            line_of_text += eval(ent + "()")
-            print(line_of_text)
-            entry()
+            if len(ent) > 6:
+                line_of_text += dered()
+                print(line_of_text)
+                entry()
+            else:
+                line_of_text += eval(ent + "()")
+                print(line_of_text)
+                entry()
         elif ent ==  "inline-code":
             line_of_text += inline_code()
             print(line_of_text)
             entry()
         elif ent == "new-line":
+            line_of_text += "\n"
             print(line_of_text)
-            print()
             entry()
         elif ent == "!done":
-            quit()
+            done(line_of_text)
         elif ent == "!help":
             help()
             entry()
