c34ee853965022142e7569d2ce5c0561e949a7aa
BrozJakub <Broz.Jk@gmail.com>
2021-08-11 12:13:37 +0200

    Webscrapper 2/5

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,18 +1,25 @@
 import requests as r
-
+from bs4 import BeautifulSoup as bs
 
 def link(url):
-    url_link = r.get(url)
+    url_link = r.get(url,headers={'Accept-Language': 'en-US,en;q=0.5'})
     if url_link.status_code == 200:
-        try:
-            print(url_link.json()["content"])
-        except BaseException:
-            print("Invalid quote resource!")
-    else:
-            print("Invalid quote resource!")
+        soup = bs(url_link.content, 'html.parser')
+        return soup
+
+
+def souping(soup):
+    result = dict()
+    result['title'] = soup.find('title').text
+    result['description'] = soup.find('meta',{'name':'description'})['content']# Napsat title and meta name description
+    return result
 
 def main():
-    link(input("Input the URL:"))
+    name = input()
+    if 'title' in name:
+        print(souping(link(name)))
+    else:
+        print('Invalid movie page!')
 
 if __name__ == "__main__":
     main()
