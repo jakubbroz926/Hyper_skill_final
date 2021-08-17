0c3d82d8576ef16b142c0e6121cd0a6d3f42b6d2
BrozJakub <Broz.Jk@gmail.com>
2021-08-17 21:24:55 +0200

    3/5 stage df handler

--- Data Analysis for Hospitals/Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/Data Analysis for Hospitals/task/analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-pd.set_option("display.max_columns", 8)
+
 
 
 def print_load(lst):
@@ -10,9 +10,11 @@ def print_load(lst):
     standard_columns = df_frames["general"].columns.to_list()
     for k, v in df_frames.items():
         v.columns = standard_columns #updating all columns names in every df
+    return df_frames
 
+def dataframes_handler(df_frames):
+#divide program into smaller functions !! TODAY 17.8.2021 !!
     new = pd.concat([df_frames["general"], df_frames["prenatal"], df_frames["sports"]], ignore_index=True)
-
     new.drop("Unnamed: 0", axis=1, inplace=True)
     new.dropna(axis=0, how="all", inplace=True) #cleaning all rows with empty values
     new["gender"] = new["gender"].replace(["male", "man"], "m")
@@ -22,4 +24,11 @@ def print_load(lst):
     print(new.sample(20, random_state=30))
 
 
-print_load([r"test/general.csv", r"test/prenatal.csv", r"test/sports.csv"])
+def main():
+    lst = [r"test/general.csv", r"test/prenatal.csv", r"test/sports.csv"]
+    frames = print_load(lst)
+    dataframes_handler(frames)
+
+if __name__ == "__main__":
+    pd.set_option("display.max_columns", 8)
+    main()
