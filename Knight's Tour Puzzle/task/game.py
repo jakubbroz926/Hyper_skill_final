4b9201607c89723674d720e00674962ef4d7434c
BrozJakub <Broz.Jk@gmail.com>
2021-08-30 13:22:49 +0200

    Knight 2/6 first try of main func

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -1,39 +1,71 @@
-def input_checking(lst_of_coord):
+def input_checking(lst_of_coord, size_of_field):
     try:
         lst_of_coord = list(map(int, lst_of_coord))
-        assert type(lst_of_coord) != list()
-        assert len(lst_of_coord) == 2
-        assert 0 < lst_of_coord[0] < 9 and 0 < lst_of_coord[1] < 9
-    except (AssertionError,ValueError):
+        assert type(lst_of_coord) != list(),"type"
+        assert len(lst_of_coord) == 2,"size"
+        assert 0 < lst_of_coord[0] <= int(size_of_field[0]) and 0 < lst_of_coord[1] <= int(size_of_field[1]),"jiná velikost"
+    except (AssertionError, ValueError):
         print("Invalid dimension!")
     else:
         return lst_of_coord
 
 
-def field(cords):
-    #ukládání pole do dalších kroků
+def dim_checking(lst_of_field):
     try:
-        x = cords[0]
-        y = cords[1]
-        line = "-"*20
-        print(line)
-        for j in range(8,0,-1):
-            print(f"{j}| ", end="")
-            for i in range(8):
-                if i == x-1 and j == y:
-                    print("X ", end = "")
-                else:
-                    print("_ ", end="")
-            print("|", end="\n")
-        print(line,end="\n   ")
-        for i in range(8):
-            print(i+1,end=" ")
-    except TypeError:
-        pass
+        field_coord = list(map(int, lst_of_field))
+        assert len(field_coord) == 2
+        assert field_coord[0] > 0 and field_coord[1] > 0
+    except (AssertionError, ValueError):
+        print("Invalid dimension!")
+    else:
+        return field_coord
+
+
+def printing(field, n_col):
+    size_of_field = n_col[1]
+    cell_size = int(len(str(n_col[0] * n_col[1])))
+
+    line = " " + "-" * (3 + n_col[0] * (cell_size + 1))
+    print(line)
+    for field_line, i in zip(field, range(size_of_field, 0, -1)):
+        print(f"{i}| ", end = "")
+        print(" ".join(field_line), end = "")
+        print(" |", end = "\n")
+    print(line, end = "\n" + " "*(cell_size+2))
+    for i in range(n_col[0]):
+        print(f"{i + 1}" + " " * cell_size, end = "")
+
+
+def def_field(sizes):
+    field_d = [[("_" * len(str(int(sizes[1]) * int(sizes[0])))) for _ in range(int(sizes[0]))] for _ in
+               range(int(sizes[1]))]
+    return field_d, sizes
+
+
+def field_change(field, cords, n_col):
+    change_field = field
+    x, y = cords[0], cords[1]
+    mark = " " * (len(str(n_col[0] * n_col[1])) - 1) + "X"
+    change_field[len(field) - y][x - 1] = mark
+    return change_field, n_col
+
+
 def main():
-    numbers = input("Enter the knight's starting position: ").split(" ")
-    cords = input_checking(numbers)
-    field(cords)
+    while True:
+        try:
+            dimensios = dim_checking(input("Enter your board dimensions: ").split(" "))
+            field_d, n_col = def_field(dimensios)
+            break
+        except TypeError:
+            print("Invalid dimension!")
+
+    while True:
+        try:
+            numbers = input_checking(input("Enter the knight's starting position: ").split(" "), dimensios)
+            printing(*field_change(field_d, numbers, n_col))
+            break
+        except TypeError:
+            print("Invalid dimension!")
 
 if __name__ == "__main__":
     main()
