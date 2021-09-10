5d0b5590ebc4ca8649fbacd6c6422f736d55d7e1
JAKUB BROZ <Broz.Jk@gmail.com>
2021-09-10 10:06:59 +0200

    101 update

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -7,7 +7,9 @@ def input_checking(lst_of_coord, size_of_field):
     except (AssertionError, ValueError):
         print("Invalid dimension!")
     else:
-        return lst_of_coord
+        lst_of_coord[0] -= 1
+        lst_of_coord[1] = size_of_field[1] - lst_of_coord[1]
+        return tuple(lst_of_coord)
 
 
 def dim_checking(lst_of_field):
@@ -39,50 +41,61 @@ def printing(field, n_col):
 
 def def_field(sizes):
     field_d = [[("_" * len(str(int(sizes[1]) * int(sizes[0])))) for _ in range(int(sizes[0]))] for _ in
-               range(int(sizes[1]))]
-    return field_d, sizes
+                   range(int(sizes[1]))]
+    return field_d
 
 
-def field_change(field, cords, n_col):
+def start_position(field, cords, rows_columns):
     change_field = field
     x, y = cords[0], cords[1]
-    mark = " " * (len(str(n_col[0] * n_col[1])) - 1) + "X"
-    change_field[len(field) - y][x - 1] = mark
-    return change_field, n_col
+    mark = " " * (len(str(rows_columns[0] * rows_columns[1])) - 1) + "X"
+    change_field[y][x] = mark
+    return change_field
 
 
-def possible_positions(field, coordinates):
+def for_possible_positions(field, coordinates):
+    coordinates = [tuple([coordinates[i],coordinates[i+1]]) for i in range(0,len(coordinates),2)]
+    possible_coordinates = ((-2, +1), (-2, -1), (-1, +2), (-1, -2), (+2, +1), (+2, -1), (+1, +2), (+1, -2))
+    x_main = coordinates[0][0]
+    y_main = coordinates[0][1]
 
-    x, y = coordinates[0]-1, len(field[0]) - coordinates[1]
-    coords_change = ((-2, 1), (-2, -1), (-1, 2), (-1, -2), (2, 1), (2, -1), (1, 2), (1, -2))
-    for i, (xi, yi) in enumerate(coords_change):
-        try:
-            if (y+yi >= 0) and (x+xi >= 0):
-                field[0][y+yi][x+xi] = "{:>2}".format(0)
-        except IndexError:
-            pass
-    return field[0]
-
+    new_coordinates = [(y_main + yi, x_main + xi) for xi, yi in possible_coordinates for y,x in coordinates
+                       if 0 <= y_main + yi < len(field) and
+                       0 <= x_main + xi < len(field[0]) and (x_main + xi, y_main + yi) != (x_main,y_main)]
+    #Potud je to ok.
+    #Je možné vytvořit rekurzi
+    for y, x in new_coordinates:
+        total = [(y+yn, x+xn) for yn,xn in possible_coordinates if
+                     0 <= y+yn < len(field) and
+                     0 <= x+xn < len(field[0]) and
+                     (x+xn,y+yn) != (x_main, y_main)]
+        field[y][x] = "{:>2}".format(len(total))
+    return field
 
 
 def main():
-    while True:
+    n = 10
+    while n != 0:
         try:
             dimensions = dim_checking(input("Enter your board dimensions: ").split(" "))
-            field_d, n_col = def_field(dimensions)
+            field_d = def_field(dimensions)
         except TypeError:
             print("Invalid dimension!")
         else:
-            while True:
+            while n != 0:
                 try:
-                    numbers = input_checking(input("Enter the knight's starting position: ").split(" "), dimensions)
-                    new_field = possible_positions(field_change(field_d, numbers, n_col), numbers)
-                    printing(new_field, n_col)
+                    # input("Enter the knight's starting position: ").split(" ") swap back before check
+                    positions = input_checking(tuple(input("Enter the knight's starting position: ").split(" ")), dimensions)
+                    start_field = start_position(field_d, positions, dimensions)
+                    new_field = for_possible_positions(start_field, positions)
+                    printing(new_field,dimensions)
                 except TypeError:
                     print("Invalid dimension!")
+                    n -= 1
                 else:
                     break
         break
+
 
 if __name__ == "__main__":
     main()
