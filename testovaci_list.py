544cba26720ade99bb67b5dfc8fcd57f504b28b9
JAKUB BROZ <Broz.Jk@gmail.com>
2021-09-14 10:40:34 +0200

    testovani_rekurze

--- testovaci_list.py
+++ testovaci_list.py
@@ -1,8 +1,14 @@
-def print_number(number):
-    number = str(number)
-    if len(number) == 0:
-        return 0
-    else:
-        return int(number[0])+print_number(number[1:])
-number = 10151561
-print(print_number(number))
+import time
+list_a = [[(1, 3), (2, 3), (2, 4)], [(1, 3), (2, 4), (4, 4)]]
+
+
+def print_list(lst):
+    for i in lst:
+        if isinstance(i, tuple):
+            print(i)
+        else:
+            print_list(i)
+
+
+print_list(list_a)
+print(time.perf_counter())
